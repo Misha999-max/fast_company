3394bb2df3a1aa60392464380275f85ba192280c
__BEAST__ <mihailkalacev9@gmail.com>
2023-01-31 16:18:53 +0300

    chenge Users fail

--- fast-company/src/component/users.jsx
+++ fast-company/src/component/users.jsx
@@ -4,29 +4,12 @@ import api from '../api'
 
 const Users = () => {
     const [users,setUsers] = useState(api.users.fetchAll())
-    const titleBGColor = users.length !== 0 ? 'bg-primary p-2' : "bg-danger" 
+    const titleBGColor = users.length !== 0 ? 'bg-primary p-2' : "bg-danger p-2" 
     console.log(users)
     const handeDelet = (id) => {
         setUsers(prevState=>prevState.filter(user=>user._id !== id))
     }
-    const renderUserRow = () => {
-        return users.length !== 0 ? users.map(user => (
-            <tr key={user._id}>
-                <th key={user.name} scope="row">{user.name}</th>
-                <th key={user.qualities._id}>{user.qualities.map(index => (
-                    <span key={index._id} className={"badge bg-" + index.color + " m-2 p-2"}>{index.name}</span>
-                ))}</th>
-                <th key={user.profession.name}>{user.profession.name}</th>
-                <th key={user.completedMeetings}>{user.completedMeetings}</th>
-                <th key={user.rate}>{user.rate}</th>
-                <th key={user._id}>
-                 <button key={Date.now()} onClick={() => handeDelet(user._id)}  className="btn btn-danger">DELETE</button>
-                </th>
-            </tr>
-        )) :
-
-        <div className="badge m-10 bg-warning">Что-то ни кто не хочет тусить</div>
-    }
+    
     return (
     <>
     
@@ -43,7 +26,24 @@ const Users = () => {
             </tr>
         </thead>
         <tbody>
-            {renderUserRow()}
+            
+       { users.length !== 0 ? users.map(user => (
+            <tr key={user._id}>
+                <th key={user.name} scope="row">{user.name}</th>
+                <th key={user.qualities._id}>{user.qualities.map(qual => (
+                    <span key={qual._id} className={"badge bg-" + qual.color + " m-2 p-2"}>{qual.name}</span>
+                ))}</th>
+                <th key={user.profession.name}>{user.profession.name}</th>
+                <th key={user.completedMeetings}>{user.completedMeetings}</th>
+                <th key={user.rate}>{user.rate}</th>
+                <th key={user._id}>
+                 <button key={Date.now()} onClick={() => handeDelet(user._id)}  className="btn btn-danger">DELETE</button>
+                </th>
+            </tr>
+        )) :
+
+        <div className="badge m-10 bg-warning">Что-то ни кто не хочет тусить</div>
+      }
         </tbody>
       </table>
     </>
